bbd7c8d7d4ddfe253e5f0e20445d339c30f137e6
Artem Laptev <artemlaptev63@mail.ru>
2020-06-23 13:05:11 +0400

    Updates

---
+++ precache-manifest.b161b66e333b1adeaabbbb0ad49de489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323bfdd71ae862eae35c3caaf8787b2d",
+    "url": "/3d-dwg-demo/index.html"
+  },
+  {
+    "revision": "d680cef2620f1a39bdee",
+    "url": "/3d-dwg-demo/static/css/main.b3efb900.chunk.css"
+  },
+  {
+    "revision": "3f70ffdae8a1a2fb992f",
+    "url": "/3d-dwg-demo/static/js/2.a0032b67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/3d-dwg-demo/static/js/2.a0032b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d680cef2620f1a39bdee",
+    "url": "/3d-dwg-demo/static/js/main.954a4738.chunk.js"
+  },
+  {
+    "revision": "680f6076c3b4be2a4c45",
+    "url": "/3d-dwg-demo/static/js/runtime-main.3cfe1ef5.js"
+  }
+]);
